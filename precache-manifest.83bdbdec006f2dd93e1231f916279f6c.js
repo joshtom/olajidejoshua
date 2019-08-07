2a942bd53aec8a7317992745fc17fb48d1515c8a
Joshua <olajidejoshua4real@gmail.com>
2019-08-07 11:52:07 +0100

    Updates

---
+++ precache-manifest.83bdbdec006f2dd93e1231f916279f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4570f041e8fb21684ea96e88cf8b46dd",
+    "url": "/joshtom/olajidejoshua/index.html"
+  },
+  {
+    "revision": "b8c6eefa6797e39e9401",
+    "url": "/joshtom/olajidejoshua/static/css/main.328f538d.chunk.css"
+  },
+  {
+    "revision": "02e0571e38897800e3ea",
+    "url": "/joshtom/olajidejoshua/static/js/2.ca293e27.chunk.js"
+  },
+  {
+    "revision": "b8c6eefa6797e39e9401",
+    "url": "/joshtom/olajidejoshua/static/js/main.0e409c4d.chunk.js"
+  },
+  {
+    "revision": "307335918684afa7c95a",
+    "url": "/joshtom/olajidejoshua/static/js/runtime~main.f807a63d.js"
+  }
+]);
