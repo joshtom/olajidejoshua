36331c5b3de3c7481e2e24cdb6f892ea7b89eb9f
Joshua <olajidejoshua4real@gmail.com>
2019-08-08 14:20:28 +0100

    Docs folder modified

---
+++ docs/precache-manifest.ef70d2cca1105b92ea235188d3ae234b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cd5e868f3e62c1488d4018f05aab5b",
+    "url": "/joshtom/olajidejoshua/index.html"
+  },
+  {
+    "revision": "43055d92e673e2da3a39",
+    "url": "/joshtom/olajidejoshua/static/css/main.328f538d.chunk.css"
+  },
+  {
+    "revision": "02e0571e38897800e3ea",
+    "url": "/joshtom/olajidejoshua/static/js/2.ca293e27.chunk.js"
+  },
+  {
+    "revision": "43055d92e673e2da3a39",
+    "url": "/joshtom/olajidejoshua/static/js/main.b8ca9e5f.chunk.js"
+  },
+  {
+    "revision": "307335918684afa7c95a",
+    "url": "/joshtom/olajidejoshua/static/js/runtime~main.f807a63d.js"
+  }
+]);
